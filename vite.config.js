09e6cf8ef4714afda2fbb04914fadc8be8569a32
ArcaIcr <morrix.ken@gmail.com>
2024-12-26 19:49:24 +0800

    casdasd

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import 'firebase/app';
-import 'firebase/firestore';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/firestore'], // Added firebase/app
-    },
   },
 })
