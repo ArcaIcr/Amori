1a75de3201ff7263e79b9acd61bbdefd40ac2d76
ArcaIcr <morrix.ken@gmail.com>
2024-12-26 18:38:14 +0800

    chore: Fix minor Issues

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['firebase/firestore'],
+      external: ['firebase/app' ,'firebase/firestore'],
     },
   },
 })
